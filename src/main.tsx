61fbd7adc1c21532b6d6e4a1674da29015f82e4b
Dhruv Anand <ananddhruv29@gmail.com>
2025-03-01 19:28:19 +0530

    Added Not Found page

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import SignupPage from "./app/signup/page.tsx";
 import LoginPage from "./app/login/page.tsx";
 import Dashboard from "./app/dashboard/page.tsx";
 import AuthProvider from "./context/AuthContext.tsx";
+import NotFound from "./app/notfound/page.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")!).render(
 					<Route path="/" element={<Dashboard />} />
 					<Route path="/signup" element={<SignupPage />} />
 					<Route path="/login" element={<LoginPage />} />
+					<Route path="/*" element={<NotFound />} />
 				</Routes>
 			</BrowserRouter>
 		</AuthProvider>
