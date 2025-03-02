7227d5263b45f827d5a02200b68839d1de800b31
Dhruv Anand <ananddhruv29@gmail.com>
2025-03-03 00:29:35 +0530

    Add sidebar menu for navigation to Payments and Transactions

--- src/app/dashboard/dashboard-sidebar.tsx
+++ src/app/dashboard/dashboard-sidebar.tsx
@@ -3,16 +3,36 @@ import {
 	SidebarContent,
 	SidebarFooter,
 	SidebarGroup,
+	SidebarGroupLabel,
 	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { Link } from "react-router-dom";
 
+// TODO: Extract sidebar data to parent layout component
 const DashboardSidebar = ({ user }: { user: User }) => {
 	return (
 		<div>
 			<Sidebar variant="floating">
 				<SidebarHeader>Payment Dashboard</SidebarHeader>
 				<SidebarContent>
-					<SidebarGroup /> {/* Add different groups here */}
+					<SidebarGroup>
+						<SidebarGroupLabel>Tables</SidebarGroupLabel>
+						<SidebarMenu>
+							<SidebarMenuItem>
+								<SidebarMenuButton asChild>
+									<Link to={"/payments"}>All Payments</Link>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+							<SidebarMenuItem>
+								<SidebarMenuButton asChild>
+									<Link to={"/transactions"}>All Transactions</Link>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+						</SidebarMenu>
+					</SidebarGroup>
 				</SidebarContent>
 				<SidebarFooter>{user.first_name}</SidebarFooter>
 			</Sidebar>
