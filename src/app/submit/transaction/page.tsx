b1ad2f72eadbe911bfd8a4feddd5d3eadec4c09a
Dhruv Anand <ananddhruv29@gmail.com>
2025-03-03 22:39:00 +0530

    Add TransactionPage component to render TransactionForm

---
+++ src/app/submit/transaction/page.tsx
@@ -0,0 +1,11 @@
+import TransactionForm from "@/components/transaction-form";
+
+const TransactionPage = () => {
+	return (
+		<div className="flex flex-col justify-center items-center h-screen w-[80vw]">
+			<TransactionForm />
+		</div>
+	);
+};
+
+export default TransactionPage;
