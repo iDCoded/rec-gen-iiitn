3890b2cb822b837b21c9ed28a17d63b5bb5b4150
Dhruv Anand <ananddhruv29@gmail.com>
2025-03-05 12:29:12 +0530

    Add JWTUser interface to define JWT token structure and extend User interface with username field

--- src/global/types.d.ts
+++ src/global/types.d.ts
@@ -1,5 +1,14 @@
+interface JWTUser {
+	token_type: string;
+	exp: number;
+	iat: number;
+	jti: string;
+	user_id: number;
+}
+
 interface User {
 	id: number;
+	username: string;
 	first_name: string;
 	last_name: string;
 	email: string;
