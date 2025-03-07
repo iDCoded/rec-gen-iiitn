f314e488a51331de39889df980b02202b80f6b1f
Dhruv Anand <ananddhruv29@gmail.com>
2025-03-07 16:19:03 +0530

    Add row actions and date formatting to payments table

--- src/app/tables/payments/columns.tsx
+++ src/app/tables/payments/columns.tsx
@@ -1,4 +1,15 @@
 import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export type Submission = {
 	submission_id: number;
@@ -24,5 +35,45 @@ export const columns: ColumnDef<Submission>[] = [
 	{
 		accessorKey: "submitted_at",
 		header: "Submitted At",
+		cell: ({ row }) => {
+			const { submitted_at } = row.original;
+			const formattedDate = new Date(submitted_at).toDateString();
+
+			return (
+				<>
+					<div>{formattedDate}</div>
+				</>
+			);
+		},
+	},
+
+	{
+		id: "actions",
+		cell: ({ row }) => {
+			const payment = row.original;
+
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" className="h-8 w-8 p-0">
+							<span className="sr-only">Open menu</span>
+							<MoreHorizontal className="h-4 w-4" />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuLabel>Actions</DropdownMenuLabel>
+						<DropdownMenuItem
+							onClick={() =>
+								navigator.clipboard.writeText(payment.submission_id.toString())
+							}>
+							Copy payment ID
+						</DropdownMenuItem>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>Approve Payment</DropdownMenuItem>
+						<DropdownMenuItem>View payment details</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			);
+		},
 	},
 ];
